24f305d80ab746c6412a9a0392e4c3a2e0e6cb8a
Anjana K G <anjana.kg@experionglobal.com>
2022-01-15 17:58:52 +0530

    files uploaded

---
+++ login/login.js
@@ -0,0 +1,38 @@
+// function checkusername()   /* This code for first name */
+// {
+// 	var uname=document.getElementById("usename").value;  
+// 	if((uname == "")||(uname.Length<=4))     // fname empty they show first name
+// 	{
+// 		document.getElementById('userM').innerHTML = "Enter valid username ";
+// 		document.getElementById('username').style.borderColor = "#FF0000";	
+// 		return false;
+// 	}
+// 	else 
+// 	{
+//         document.getElementById('username').style.borderColor = "#228B22";
+// 		document.getElementById('userM').innerHTML = "";
+//     }
+// 	return true;
+// }
+
+// function checkpswrd()   /* This code for first name */
+// {
+// 	var pswd=document.getElementById("password").value;  
+// 	if((pswrd == "")||(pswrd.Length<=4))     // fname empty they show first name
+// 	{
+// 		document.getElementById('pass').innerHTML = "Enter valid username ";
+// 		document.getElementById('password').style.borderColor = "#FF0000";	
+// 		return false;
+// 	}
+// 	else 
+// 	{
+//         document.getElementById('password').style.borderColor = "#228B22";
+// 		document.getElementById('pass').innerHTML = "";
+//     }
+// 	return true;
+// }
+function validate(){
+    location.href="./dashboard/home.html"  
+}
+
+
